2b0e8f0ba23caab737e87c13cf3303a2332be9fb
LX0210 <2580617135@qq.com>
2025-09-03 00:20:12 +0800

    add a a

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" <<"how are you?"<< std::endl;
+    std::cout << "Hello, World!" <<"how are you?"<<"i am fine,thank you"<< std::endl;
     system( "pause" );
     return 0;
 }
