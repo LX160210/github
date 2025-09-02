692a9aa2da3d6892d9f3098ced00cbef7b563b83
LX0210 <2580617135@qq.com>
2025-09-03 00:09:43 +0800

    add a greet

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" <<"how are you?"<< std::endl;
     system( "pause" );
     return 0;
 }
